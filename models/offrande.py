a1e9f76ad418d7ae3a319444efab93f9a30cff54
Silakiniaina <153703946+Silakiniaina@users.noreply.github.com>
2024-03-05 12:37:58 +0300

    Creation fonctoin set_id + Exception

--- models/offrande.py
+++ models/offrande.py
@@ -1,7 +1,13 @@
-
+from exception import *
 class Offrande: 
     id: int
     montant: float
     numero_dimanche: int 
     annee: int 
-    id_eglise: int 
+    id_eglise: int
+     
+    def set_id(self,entered_id: int):
+        if(entered_id <= 0):
+            raise NullOrNegativeNumberException("The id can't be null or less than 0")
+        else: 
+            self.id = entered_id
