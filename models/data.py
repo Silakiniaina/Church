8b8fb40e19132efa1be5f6b59e172ebc171eed54
Silakiniaina <153703946+Silakiniaina@users.noreply.github.com>
2024-03-05 10:43:06 +0300

    Creation function get_connection dans Database

--- models/data.py
+++ models/data.py
@@ -2,3 +2,11 @@ import pyodbc
 
 class Database:
     
+    @staticmethod   
+    def get_connection() -> pyodbc.Connection:
+        connection_string = 'DRIVER=ODBC Driver 17 for SQL Server;SERVER=DESKTOP-8QBE9Q1;DATABASE=church;UID=Sanda;PWD=DashDashGo2K23!!'
+        try: 
+            connection = pyodbc.connect(connection_string)
+        except Exception as e:
+            raise e
+        return connection
