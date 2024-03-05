ce590acd71109de0b2d5e9feac91db948ae5684d
Silakiniaina <153703946+Silakiniaina@users.noreply.github.com>
2024-03-05 12:10:54 +0300

    Creation fonction set_id + Exception dans Eglise

--- models/eglise.py
+++ models/eglise.py
@@ -3,4 +3,9 @@ class Eglise:
     id: int
     name: str
     
+    def set_id(self,entered_id: int):
+        if(entered_id <= 0):
+            raise NullOrNegativeNumberException("The id can't be null or less than 0")
+        else: 
+            self.id = entered_id
     
