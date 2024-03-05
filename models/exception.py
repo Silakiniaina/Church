aba2f482cfc90aadc61b7185b537c48d2fe9f7e2
Silakiniaina <153703946+Silakiniaina@users.noreply.github.com>
2024-03-05 10:46:34 +0300

    Creation classe : EmailException, DateBirthException, EmptyNameException, NullOrNegativeNumberException dans la module exception

---
+++ models/exception.py
@@ -0,0 +1,11 @@
+class EmailException(Exception):
+    pass
+
+class DateBirthException(Exception):
+    pass
+
+class EmptyNameException(Exception):
+    pass
+
+class NullOrNegativeNumberException(Exception):
+    pass
