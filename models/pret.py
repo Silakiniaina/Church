58113b107a5465b1846c0db8b923d596b2f787cc
Silakiniaina <153703946+Silakiniaina@users.noreply.github.com>
2024-03-06 04:06:56 +0300

    Creation setter montant dans la classe pret

--- models/pret.py
+++ models/pret.py
@@ -6,5 +6,10 @@ class Pret:
     date_pret: datetime
     montant: float 
     id_croyant: int
-
+    
+    def set_montant(self,entered_montant: float):
+        if(entered_montant < 0):
+            raise NumberException("Le montant ne peut pas etre negatif")
+        else:
+            self.montant = entered_montant 
     
